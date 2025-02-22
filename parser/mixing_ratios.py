75c3e349c72d37239f26c74ce0723232b0649761
Brandonaerys <brandonaerys@gmail.com>
2025-02-22 18:12:14 +0000

    mixing_ratios cleaned

--- parser/mixing_ratios.py
+++ parser/mixing_ratios.py
@@ -2,53 +2,27 @@
 
 # sample usage:
 # python mixing_ratios.py ../output/GasDwarf.vul H2O,CH4,CO,N2,H2,CO2,NH3,H2S,HCN,CS2 GasDwarf_incomplete 1e-4 1e-1 -r
-import sys
-sys.path.insert(0, '../') # including the upper level of directory for the path of modules
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.legend as lg
-import vulcan_cfg
-try: from PIL import Image
-except ImportError:
-    try: import Image
-    except: vulcan_cfg.use_PIL = False
-import os, sys
 import pickle
-
-
+import os, sys
 
 # inputs: vul_data as .vul file, plot spec as single comma-separated string, pressures as list of min/max pressure, plot_name as string
-
+# outputs: array of species labels, array of mixing ratios
 def mixing_ratios(vul_data,spec,plot_name,min_pressure_bar,max_pressure_bar=1,use_range=True, plot_save=True):
 
-
-
-    plot_dir = '../' + 'parser_output'
-    # Checking if the plot folder exsists
-    if not os.path.exists(plot_dir):
-        print( 'Directory ' , plot_dir,  " created.")
-        os.mkdir(plot_dir)
 
     # taking user input species and splitting into separate strings and then converting the list to a tuple
     spec = tuple(spec.split(','))
     nspec = len(spec)
 
-    # These are the "Tableau 20" colors as RGB.
-    tableau20 = [(31, 119, 180),(255, 127, 14),(44, 160, 44),(214, 39, 40),(148, 103, 189),(140, 86, 75), (227, 119, 194),(127, 127, 127),(188, 189, 34),(23, 190, 207),\
-    (174, 199, 232),(255, 187, 120),(152, 223, 138),(255, 152, 150),(197, 176, 213),(196, 156, 148),(247, 182, 210),(199, 199, 199),(219, 219, 141),(158, 218, 229)]
-    #
 
-
-    # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.
-    for i in range(len(tableau20)):
-        r, g, b = tableau20[i]
-        tableau20[i] = (r / 255., g / 255., b / 255.)
-
-    # # tex labels for plotting
-    # tex_labels = {'H':'H','H2':'H$_2$','O':'O','OH':'OH','H2O':'H$_2$O','CH':'CH','C':'C','CH2':'CH$_2$','CH3':'CH$_3$','CH4':'CH$_4$','HCO':'HCO','H2CO':'H$_2$CO', 'C4H2':'C$_4$H$_2$',\
-    # 'C2':'C$_2$','C2H2':'C$_2$H$_2$','C2H3':'C$_2$H$_3$','C2H':'C$_2$H','CO':'CO','CO2':'CO$_2$','He':'He','O2':'O$_2$','CH3OH':'CH$_3$OH','C2H4':'C$_2$H$_4$','C2H5':'C$_2$H$_5$','C2H6':'C$_2$H$_6$','CH3O': 'CH$_3$O'\
-    # ,'CH2OH':'CH$_2$OH','N2':'N$_2$','NH3':'NH$_3$', 'NO2':'NO$_2$','HCN':'HCN','NO':'NO', 'NO2':'NO$_2$' }
+    # tex labels for plotting
+    tex_labels = {'H':'H','H2':'H$_2$','O':'O','OH':'OH','H2O':'H$_2$O','CH':'CH','C':'C','CH2':'CH$_2$','CH3':'CH$_3$','CH4':'CH$_4$','HCO':'HCO','H2CO':'H$_2$CO', 'C4H2':'C$_4$H$_2$',\
+    'C2':'C$_2$','C2H2':'C$_2$H$_2$','C2H3':'C$_2$H$_3$','C2H':'C$_2$H','CO':'CO','CO2':'CO$_2$','He':'He','O2':'O$_2$','CH3OH':'CH$_3$OH','C2H4':'C$_2$H$_4$','C2H5':'C$_2$H$_5$','C2H6':'C$_2$H$_6$','CH3O': 'CH$_3$O'\
+    ,'CH2OH':'CH$_2$OH','N2':'N$_2$','NH3':'NH$_3$', 'NO2':'NO$_2$','HCN':'HCN','NO':'NO', 'NO2':'NO$_2$', 'H2S':'H$_2$S', 'CS2':'CS$_2$'}
 
     with open(vul_data, 'rb') as handle:
       data = pickle.load(handle)
@@ -64,13 +38,13 @@ def mixing_ratios(vul_data,spec,plot_name,min_pressure_bar,max_pressure_bar=1,us
         if len(selected_indices) == 0:
             print(f"Error: No data points found in the pressure range {min_pressure_bar} - {max_pressure_bar} bar.")
             sys.exit(1)
-        print(f"Plotting data averaged over pressure range: {min_pressure_bar:.3e} - {max_pressure_bar:.3e} bar")
+        print(f"data averaged over pressure range: {min_pressure_bar:.3e} - {max_pressure_bar:.3e} bar")
     else:
         # Find the closest pressure index
         closest_idx = np.argmin(np.abs(pressure_array - min_pressure_bar))
         selected_indices = [closest_idx]
 
-        print(f"Plotting data at closest pressure: {pressure_array[closest_idx]:.3e} bar")
+        print(f"data at closest pressure: {pressure_array[closest_idx]:.3e} bar")
 
     # Prepare data for bar chart
     mixing_ratios = []
@@ -80,12 +54,22 @@ def mixing_ratios(vul_data,spec,plot_name,min_pressure_bar,max_pressure_bar=1,us
         if sp in vulcan_spec:
             avg_ratio = np.mean(data['variable']['ymix'][selected_indices, vulcan_spec.index(sp)])
             mixing_ratios.append(avg_ratio)
-            species_labels.append(sp)
+            if sp in tex_labels:
+                species_labels.append(tex_labels[sp])
+            else:
+                species_labels.append(sp)
         else:
             print(f"Warning: {sp} not found in the dataset.")
 
     # Plot bar chart
     if plot_save:
+        plot_dir = '../parser_output/mixing_ratios'
+        # Checking if the plot folder exsists
+        if not os.path.exists(plot_dir):
+            print( 'Directory ' , plot_dir,  " created.")
+            os.makedirs(plot_dir)
+
+
         plt.figure(figsize=(10, 6))
         plt.bar(species_labels, mixing_ratios, log=True)
 
@@ -99,10 +83,8 @@ def mixing_ratios(vul_data,spec,plot_name,min_pressure_bar,max_pressure_bar=1,us
         plt.savefig(os.path.join(plot_dir, plot_name + '.png'))
         plt.savefig(os.path.join(plot_dir, plot_name + '.eps'))
 
-        if vulcan_cfg.use_PIL:
-            Image.open(os.path.join(plot_dir, plot_name + '.png')).show()
-        else:
-            plt.show()
+
+        plt.show()
 
 
         return species_labels, mixing_ratios
