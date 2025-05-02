1bd45c9dd6466a7898369a8e0a490019d5dc4f9c
Brandonaerys <brandonaerys@gmail.com>
2025-05-02 17:48:07 +0100

    more hycean runs 0205

--- parser/rf.py
+++ parser/rf.py
@@ -47,9 +47,10 @@ with np.errstate(divide='ignore'):
 X = np.array(dfs)
 y = np.array(labels)
 
-print(X)
-print(y)
-exit()
+# print(len(y))
+# print(X)
+# print(y)
+# exit()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=seed)
 
